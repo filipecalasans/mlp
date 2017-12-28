1c786e7616a19120eb78c25ecf40287984cbb4a1
Filipe Calasans <filipe.calasans@gmail.com>
2017-12-28 17:17:05 -0500

    implement iris example using minibatch and normalization of inputs

---
+++ iris-mlp-minibatch.py
@@ -0,0 +1,33 @@
+from lib.mlp import NeuralNetwork
+import numpy as np
+
+if __name__ == "__main__":
+   
+   print("MLP Test usin XOR gate example")   
+
+   filename = "iris.data"
+
+   dataset = np.loadtxt(open(filename, "rb"), delimiter=",")
+
+   output_size = 3
+   input_size = dataset.shape[1] - output_size
+
+   print("======= Dataset =========\n{}".format(dataset))
+
+   
+   max_col = np.amax(dataset, axis=0)
+   min_col = np.amin(dataset, axis=0)
+
+   dataset = dataset/(max_col - min_col)
+
+   print("MAX: {}, MIN: {}".format(max_col, min_col))
+
+   nn_size = [input_size, 5, output_size]
+
+   mlp = NeuralNetwork(nn_size, True)
+
+   batch_size = 1
+   mlp.train(dataset, eta=0.01, threshold=1e-3)
+
+   a, y = mlp.classify(dataset[63][0:input_size])
+   print("Y: {}, Ŷ: {}".format(dataset[63][-(input_size-1):], y))
