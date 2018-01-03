normalization fix

iris-mlp-minibatch.py
@@ -13,12 +13,11 @@ if __name__ == "__main__":
    input_size = dataset.shape[1] - output_size
 
    print("======= Dataset =========\n{}".format(dataset))
-
    
    max_col = np.amax(dataset, axis=0)
    min_col = np.amin(dataset, axis=0)
 
-   dataset = dataset/(max_col - min_col)
+   dataset = (dataset-min_col)/(max_col - min_col)
 
    print("MAX: {}, MIN: {}".format(max_col, min_col))
 
@@ -27,7 +26,7 @@ if __name__ == "__main__":
    mlp = NeuralNetwork(nn_size, True)
 
    batch_size = 10
-   mlp.train(dataset, eta=0.05, threshold=3e-2)
+   mlp.train(dataset, eta=0.05, threshold=1e-3)
 
    a, y = mlp.classify(dataset[63][0:input_size])
 